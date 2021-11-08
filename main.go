34b5a23e5460ed4e152c54d464c65e0c0600ff13
R. G. Esteves <rgesteves@gmail.com>
2021-11-08 05:36:38 +0000

    Basic `wc` implementation

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var cpuCount = runtime.NumCPU()
+	fmt.Printf("Running with %d cores\n", cpuCount) // not sure why `Println` does not take %d-style specifiers
+	fmt.Println("Calling the function count")
+	fmt.Printf("The result of calling count is [%d]\n", count(os.Stdin))
+	fmt.Println("Done!")
+}
+
+func count(r io.Reader) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
+
+	// Define the scanner split type to words (default is split by lines)
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
+	return wc
 }
